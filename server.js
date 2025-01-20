2584f7c6007ae94b66335d899567bbb854d3a4e4
rajukumargiri <rajukumargiri123@gmail.com>
2025-01-20 21:23:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-npm run devstart
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/angular-trading-platform')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/angular-trading-platform/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
